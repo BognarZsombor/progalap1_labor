2c904a50b457fcf818e1f6d9b2a58ce7b83cda69
D <bzsombi99@gmail.com>
2021-09-20 15:55:23 +0200

    3ora_fel alap

---
+++ 3ora_fel/cmake-build-debug/CMakeFiles/3ora_fel.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3ora_fel.exe"
+  "3ora_fel.pdb"
+  "CMakeFiles/3ora_fel.dir/main.c.o"
+  "lib3ora_fel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3ora_fel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
