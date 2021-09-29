1c79679cce1cad6dbdba79f9f3ad405057441793
D <52254412+BognarZsombor@users.noreply.github.com>
2021-09-29 17:11:31 +0200

    gyak_szorg_bankautomata kész

---
+++ gyak_szorg_bankautomata/cmake-build-debug/CMakeFiles/gyak_szorg_bankautomata.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gyak_szorg_bankautomata.dir/main.c.o"
+  "gyak_szorg_bankautomata.exe"
+  "gyak_szorg_bankautomata.pdb"
+  "libgyak_szorg_bankautomata.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gyak_szorg_bankautomata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
