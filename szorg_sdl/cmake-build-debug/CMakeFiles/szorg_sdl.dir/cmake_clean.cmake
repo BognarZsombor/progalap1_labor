8855316d2726a256a9c5a04f6db6f867b8ba0096
D <52254412+BognarZsombor@users.noreply.github.com>
2021-10-15 11:25:26 +0200

    szorg_sdl alap

---
+++ szorg_sdl/cmake-build-debug/CMakeFiles/szorg_sdl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/szorg_sdl.dir/main.c.obj"
+  "libszorg_sdl.dll.a"
+  "szorg_sdl.exe"
+  "szorg_sdl.exe.manifest"
+  "szorg_sdl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/szorg_sdl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
