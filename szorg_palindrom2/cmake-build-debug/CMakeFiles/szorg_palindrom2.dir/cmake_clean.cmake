d29addebbe293da862ae608408f879f03ca5ec94
D <52254412+BognarZsombor@users.noreply.github.com>
2021-10-22 16:47:42 +0200

    szorg_palindrom kész

---
+++ szorg_palindrom2/cmake-build-debug/CMakeFiles/szorg_palindrom2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/szorg_palindrom2.dir/main.c.obj"
+  "libszorg_palindrom2.dll.a"
+  "szorg_palindrom2.exe"
+  "szorg_palindrom2.exe.manifest"
+  "szorg_palindrom2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/szorg_palindrom2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ szorg_palindrom2/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "string.h"
+#include "stdbool.h"
+
+bool palindrom(char text[]) {
+    int hossz = strlen(text);
+    int i = 0;
+    int j = hossz - 1;
+    bool volt_hiba = false;
+
+    if (!isalpha(text[j])) {
+        j--;
+    }
+
+    while (i < hossz && j >= 0 && !volt_hiba) {
+        if (text[i] == ' ')
+            i++;
+        else if (text[j] == ' ')
+            j--;
+        else if (text[i++] != text[j--])
+            volt_hiba = true;
+    }
+
+
+}
+
+int main() {
+    char text[50+1];
+    printf("Szoveg: ");
+    scanf("%50[^\n]", text);
+
+    for(int i = 0; text[i]; i++){
+        text[i] = tolower(text[i]);
+    }
+
+    palindrom(text) ? printf("Palindrom.") : printf("Nem palindrom.");
+
+    return 0;
+}
